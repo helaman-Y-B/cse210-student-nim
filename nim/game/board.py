e1e88f873513e5f0e787d3108709a55307a4a833
helaman-Y-B <helamashi@gmail.com>
2021-10-20 20:28:01 -0300

    Trying to make the apply function

--- nim/game/board.py
+++ nim/game/board.py
@@ -7,22 +7,15 @@ class Board:
     def __init__(self) -> None:
         self._piles = []
 
-    def apply(self):
+    def apply(self, move):
         """The apply method applies a move to the playing surface. In this case, that means removing a number of stones from a pile. The apply method accepts one argument, an instance of Move."""
 
         # You will need to separate each 'o' inside the list
-        if self.choice == '1':
-            self._piles[0] = 'o'*self.user_play
-        elif self.choice == '2':
-            self._piles[1] = 'o'*self.user_play
-        elif self.choice == '3':
-            self._piles[2] = 'o'*self.user_play
-        elif self.choice == '4':
-            self._piles[3] = 'o'*self.user_play
-        elif self.choice == '5':
-            self._piles[4] = 'o'*self.user_play
-        else:
-            print("There is no corresponding number")
+        for i in range(len(self._piles)):
+            if i == move:
+                for j in i:
+                    for _ in range(move):
+                        j.pop()
 
     def is_empty(self):
         """The is_empty method determines if all the stones have been removed from the board. It returns True if the board has no stones on it; false if otherwise."""
@@ -39,6 +32,14 @@ class Board:
 
     def _prepare(self):
         """The _prepare method sets up the board with a random number of piles (2 - 5) containing a random number of stones (1 - 9)."""
-        piles = random.randint(2, 5)
-        for i in range(piles):
-            self._piles.append(str(i))
+        self.piles = random.randint(2, 5)
+        for _ in range(self.piles):
+            random_number = random.randint(1, 9)
+            rocks = "o"*random_number
+            split = list(rocks)
+            self._piles.append(split)
+
+
+board = Board()
+board._prepare()
+board.to_string()
